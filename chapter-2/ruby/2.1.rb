cda7c444e22f0d05903ccc036814d10479f91eb3
Evandro Leopoldino Gonalves <evandrolgoncalves@gmail.com>
2017-03-20 21:36:48 -0300

    add solution to problem 2.1

---
+++ chapter-2/ruby/2.1.rb
@@ -0,0 +1,21 @@
+def remove_duplacate_nodes node
+  unless node[:node_next]
+    return
+  end
+
+  duplicates = []
+  duplicates.push node[:data]
+
+  current = node[:node_next]
+
+  while current do
+    if duplicates.include? current[:data]
+      current[:node_prev][:node_next] = current[:node_next]
+      current[:node_next][:node_prev] = current[:node_prev]
+    else
+      duplicates.push current[:data]
+    end
+
+    current = current[:node_next]
+  end
+end

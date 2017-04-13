01977a4d93ee4e8ef773133c1cd663f89444e38d
Evandro Leopoldino Gonalves <evandrolgoncalves@gmail.com>
2017-04-13 14:29:34 -0300

    update js solution to problem 2.2

--- chapter-2/js/2.2.js
+++ chapter-2/js/2.2.js
@@ -3,37 +3,64 @@ Implement an algorithm to find the nth to last element of a singly linked list.
 */
 
 // Complexity O(N)
-var getNodeByIndex = function(head, index) {
-  var current = head;
-  var i = 0;
+function nthToLast(head, nth) {
+  if (!head || nth < 1) { return; }
 
-  while (i < index) {
-    current = current.next;
-    i = i + 1;
+  p1 = head;
+
+  for (var i=0; i < nth - 1; i++) {
+    if (!p1.nodeNext) { return; }
+    p1 = p1.nodeNext;
   }
 
-  return current; 
+  p2 = head;
+
+  while(p1.nodeNext) {
+    p1 = p1.nodeNext;
+    p2 = p2.nodeNext;
+  }
+
+  return p2;
+}
+
+// Testing
+var assert = require('assert');
+
+function LinkedList() {
+  var size = 0;
+  var head = null;
+  var tail = null;
 };
 
+LinkedList.prototype = {
+  _createNode: function(data) {
+    return {
+      nodeNext: null,
+      data: data
+    };
+  },
 
-// Complexity O(N)
-var findNthToLastElement = function(headNode, nth) {
-  if (typeof(nth) != 'number' || nth < 0) { return false; }
+  addNode: function(data) {
+    var node = this._createNode(data);
+    this.size = this.size + 1;
 
-  var nthNode = getNodeByItem(headNode, nth);
+    if (!this.head) {
+      this.head = node;
+    } else {
+      this.tail.nodeNext = node;
+    }
 
-  if (nthNode == null) {
-    return [];
+    this.tail = node;
   }
-
-  var output = [];
-  var current = nthNode.next;
-  output.push(current);
-
-  while (current != null) {
-    output.push(current);
-    current = current.next;
-  }
-
-  return output;
 };
+
+var linked = new LinkedList();
+var a = linked.addNode('a');
+var b = linked.addNode('b');
+var c = linked.addNode('c');
+var d = linked.addNode('d');
+var e = linked.addNode('e');
+var f = linked.addNode('f');
+
+assert.equal(e, nthToLast(a, 2));
+assert.equal(d, nthToLast(a, 3));
